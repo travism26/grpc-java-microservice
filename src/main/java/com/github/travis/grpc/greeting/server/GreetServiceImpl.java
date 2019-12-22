d8846d52acd0801683a1673bde402656653f44d4
travism26 <travismartin266@gmail.com>
2019-12-22 14:04:11 -0400

    add clean up and added the async call for the client streaming on server side

--- src/main/java/com/github/travis/grpc/greeting/server/GreetServiceImpl.java
+++ src/main/java/com/github/travis/grpc/greeting/server/GreetServiceImpl.java
@@ -54,4 +54,36 @@ public class GreetServiceImpl extends GreetServiceGrpc.GreetServiceImplBase {
         }
 
     }
+
+    @Override
+    public StreamObserver<LongGreetRequest> longGreet(StreamObserver<LongGreetResponse> responseObserver) {
+        StreamObserver<LongGreetRequest> requestObserver = new StreamObserver<LongGreetRequest>() {
+            String result = "";
+            @Override
+            public void onNext(LongGreetRequest value) {
+                // Client sends a message
+                result += "Whats up " + value.getGreeting().getFirstName() + "! ";
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                // Client sends an Error
+            }
+
+            @Override
+            public void onCompleted() {
+                // Client is done
+
+                // this is when we want to return a response (ResponseObserver)
+                responseObserver.onNext(
+                        LongGreetResponse.newBuilder()
+                                .setResult(result)
+                                .build()
+                );
+            }
+        };
+//        return super.longGreet(responseObserver)
+
+        return requestObserver;
+    }
 }
