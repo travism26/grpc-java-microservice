48a6f43b55d1d333398f975816025ee82db223b9
travism26 <travismartin266@gmail.com>
2020-01-15 21:27:06 -0400

    update build script

--- protoc_build.sh
+++ protoc_build.sh
@@ -1,12 +1,14 @@
 ROOTDIR=`pwd`
-#brew install protobuf
+
+# Uncomment below line to install protobuf for MAC OS only...
+# brew install protobuf
 function build_client_js_files() {
-    protoc --proto_path=proto/blog --js_out=import_style=commonjs,binary:client/src/ --grpc-web_out=import_style=commonjs,mode=grpcwebtext:client/src/ proto/blog/blog.proto
+  echo "Building Client side proto files..."
+  protoc --proto_path=proto/blog --js_out=import_style=commonjs,binary:client/src/ --grpc-web_out=import_style=commonjs,mode=grpcwebtext:client/src/ proto/blog/blog.proto
 }
 
 function build_java_class() {
-    cd $ROOTDIR
-     echo "Running gradlew generateProto `./gradlew generateProto`"
+  echo "Running gradlew generateProto... `./gradlew generateProto`"
 }
 
 build_client_js_files
